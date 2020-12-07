1f01f151aee16bca7034b476ac432e212dc04180
ilopezchavez <ilopezchavez@ucmerced.edu>
2020-12-07 15:25:04 -0800

    Interface skeleton code

---
+++ phase 2/Interface.py
@@ -0,0 +1,46 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def openConnection(_dbFile):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Open database: ", _dbFile)
+
+    conn = None
+    try:
+        conn = sqlite3.connect(_dbFile)
+        print(" success")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+    return conn
+
+def closeConnection(_conn, _dbFile):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Close database: ", _dbFile)
+
+    try:
+        _conn.close()
+        print("success")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+def main():
+    database = r"phase 2/NFLstats.sqlite"
+
+    # create a database connection
+    conn = openConnection(database)
+    # with conn:
+    #     dropTable(conn)
+    #     createTable(conn)
+    #     populateTable(conn)
+
+    closeConnection(conn, database)
+
+
+if __name__ == '__main__':
+    main()
