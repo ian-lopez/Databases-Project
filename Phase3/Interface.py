2626cef327fbee1a3f6ea70cbcc0b68e27018b61
ilopezchavez <ilopezchavez@ucmerced.edu>
2020-12-07 20:59:30 -0800

    Base functions added

--- Phase3/Interface.py
+++ Phase3/Interface.py
@@ -29,6 +29,55 @@ def closeConnection(_conn, _dbFile):
 
     print("++++++++++++++++++++++++++++++++++")
 
+def setup(_conn):
+    out = """Please enter in a choice that you want to complete:
+    1. Insert Player
+    2. Update Information
+    3. Delete Information
+    4. Search Stats"""
+
+    print(out)
+    choice = input()
+    
+    if (choice == '1'):
+        insert(_conn)
+    elif (choice == '2'):
+        update(_conn)
+    elif (choice == '3'):
+        delete(_conn)
+    elif (choice == '4'):
+        search(_conn)
+    else:
+        print("Please try again with a valid choice")
+
+def insert(_conn):
+    try:
+        sql = """insert into players()"""
+
+    except Error as e:
+        print(e)
+
+def update(_conn):
+    try:
+        sql = ""
+
+    except Error as e:
+        print(e)
+
+def delete(_conn):
+    try:
+        sql = ""
+
+    except Error as e:
+        print(e)
+
+def search(_conn):
+    try:
+        sql = ""
+
+    except Error as e:
+        print(e)
+
 def main():
     database = r"phase 2/NFLstats.sqlite"
 
@@ -38,6 +87,8 @@ def main():
     #     dropTable(conn)
     #     createTable(conn)
     #     populateTable(conn)
+
+    setup(conn)
 
     closeConnection(conn, database)
 
